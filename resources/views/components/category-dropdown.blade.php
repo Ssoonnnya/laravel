92c232de700de043ed16baa34e4c5aeb154ee763
Sonya <SofiKonors@gmail.com>
2023-09-21 00:56:52 +0300

    ep.42

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -1,7 +1,7 @@
 <x-dropdown>
     <x-slot name="trigger">
         <button class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left flex lg:inline-flex">
-            
+
             {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories' }}
 
             <x-icon name="down-arrow" class="absolute pointer-events-none" style="right: 12px;"/>
@@ -14,10 +14,9 @@
     @foreach ($categories as $category)
 
         <x-dropdown-item
-
-            href="/categories/{{ $category->slug }}"
+            href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category', 'page')) }}"
             :active='request()->is("categories/{$category->slug}")'
-
-            >{{ ucwords($category->name) }}</x-dropdown-item>
+        >{{ ucwords($category->name) }}</x-dropdown-item>
     @endforeach
+
 </x-dropdown>
