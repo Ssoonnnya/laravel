6433e697597ab5ebfbb9c636510eddc73006ab7d
Sonya <SofiKonors@gmail.com>
2023-09-21 15:34:32 +0300

    ep.47-48 but need a little fix

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -18,8 +18,14 @@
                            type="text"
                            name="name"
                            id="name"
+                           value="{{ old('name') }}"
                            required
                     >
+
+                    @error('name')
+                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                    @enderror
+
                 </div>
 
                 <div class="mb-6">
@@ -34,8 +40,13 @@
                            type="text"
                            name="username"
                            id="username"
+                           value="{{ old('username') }}"
+
                            required
                     >
+                    @error('username')
+                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -50,8 +61,12 @@
                            type="email"
                            name="email"
                            id="email"
+                           value="{{ old('email') }}"
                            required
                     >
+                    @error('email')
+                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -68,6 +83,9 @@
                            id="password"
                            required
                     >
+                    @error('password')
+                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
