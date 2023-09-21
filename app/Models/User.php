6c6c09b9d994b5efafbf7d71b8dfbf0d55f292c2
Sonya <SofiKonors@gmail.com>
2023-09-21 14:04:35 +0300

    ep.45-46

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,16 +13,10 @@ class User extends Authenticatable
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
-     * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
+    protected $guarded = [];
     /**
      * The attributes that should be hidden for serialization.
      *
@@ -42,6 +36,11 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function setPasswordAttribute($password){
+        
+        $this->$attributes['password'] = bcrypt($password);
+    }
 
     public function posts(){
         return $this->hasMany(Post::class);
--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,16 @@ use Illuminate\Support\Facades\Route;
 use App\Models\Post;
 use App\Models\Category;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
 
 
 Route::get('/',[PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}',[PostController::class,'show']);
+
+Route::get('register', [RegisterController::class, 'create']);
+Route::post('register', [RegisterController::class, 'store']);
+
 
 
 
