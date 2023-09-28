3d01edaa1fcbb9f83bab95c1d9e182bff0a19147
Sonya <SofiKonors@gmail.com>
2023-09-28 14:38:35 +0300

    ep.59

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,22 @@ use App\Http\Controllers\SessionController;
 use App\Http\Controllers\PostCommentsController;
 
 
+Route::get('ping', function(){
+
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us9'
+    ]);
+
+    $response = $mailchimp->lists->addListMember("ac0012cb00",[
+        
+    ]);
+        ddd($response);
+
+});
+
 Route::get('/',[PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}',[PostController::class,'show']);
