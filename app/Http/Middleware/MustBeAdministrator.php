e8394d6a0d4ffce5942740fb62b57f98a6261508
Sonya <SofiKonors@gmail.com>
2023-10-02 13:13:44 +0300

    ep.63-64

--- app/Http/Middleware/MustBeAdministrator.php
+++ app/Http/Middleware/MustBeAdministrator.php
@@ -15,7 +15,7 @@ class MustBeAdministrator
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (auth()->user()?->username !== 'sonya') {
+        if (auth()->user()?->username !== 'Ssooonnnya') {
             abort(Response::HTTP_FORBIDDEN);
         }
 
